3961036e0d07b4d41b94b2d6eec6bfd6b1d00b02
Eric Spahr <ebspahr@gmail.com>
2020-04-27 17:37:15 -0500

    119

--- in_progress/119.jl
+++ in_progress/119.jl
@@ -1,70 +1,70 @@
-# 30 sec to find # 16 starting at # 15
-function is_interesting_old(n::Int64)
-    sume::Int64 = sum( digits(n, base = 10) )
-    pow::Int64 = 2
-    interesting::Bool = false
-    while true
-        if sume < 2
-            return false
-        end
-        if sume ^ pow == n
-            return true
-        elseif sume ^ pow > n
-            return false
-        end
-        pow += 1
-    end
-end
-
-# 37 sec to find # 16 starting at # 15
 function is_interesting(n::Int64)
-    sume::Int64 = sum( digits(n, base = 10) ) 
-    p::Int64 = 0
+    digit_sum::Int64 = sum( digits(n, base = 10) )
+    if digit_sum < 2
+        return false
+    end
     while true
-        if sume < 2
-            return false
-        end
-        numbre::Float64 = n / (sume ^ p)
-        if numbre == 1
+        if n == 1
             return true
         end
-        if numbre % sume != 0
+        if n % digit_sum == 0
+            n::Float64 = n / digit_sum
+        else
             return false
         end
-        p += 1
     end
 end
 
-function doo()
-    count::Int64 = 0
-    n::Int64 = 60466177
-    while count < 1
-        if is_interesting_old(n) == true
-            count += 1
-            println(count, "  ", n)
+function check_nums(start::Int64, stop::Int64)
+    for n::Int64 = start:stop
+        if is_interesting(n)
+            println(n)
         end
-        n += 1
     end
 end
 
-@time doo()
+function distribute(init::Int64, end_point::Int64)
+    println("threadcount ", Threads.nthreads())
+    batch_size::Int64 = 10000000
+    batches = convert(Int64, floor( (end_point - init) / batch_size))
+    if batches < 1
+        batches = 1
+    end
+    @sync for i::Int64 = 1:batches
+        start::Int64 = (i-1) * batch_size + init
+        stop::Int64 = i * batch_size + init
+        if start < 10
+            start = 11
+        end
+        if stop > end_point
+            stop = end_point
+        end
+        Threads.@spawn check_nums(start, stop)
+    end
+    check_nums(batches * batch_size, end_point)
+end
 
-# is_interesting(29)
+@time distribute(12000000000, 13000000000)
 
-# 1  81
-# 2  512
-# 3  2401
-# 4  4913
-# 5  5832
-# 6  17576
-# 7  19683
-# 8  234256
-# 9  390625
-# 10  614656
-# 11  1679616
-# 12  17210368
-# 13  34012224
-# 14  52521875
-# 15  60466176
-# 16  205962976
-# 17  612220032
+# through 12,000,000,000
+
+# 1 - 81
+# 2 - 512
+# 3 - 2401
+# 4 - 4913
+# 5 - 5832
+# 6 - 17576
+# 7 - 19683
+# 8 - 234256
+# 9 - 390625
+# 10 - 614656
+# 11 - 1679616
+# 12 - 17210368
+# 13 - 34012224
+# 14 - 52521875
+# 15 - 60466176
+# 16 - 205962976
+# 17 - 612220032
+# 18 - 8303765625
+# 19 - 10460353203
+
--- in_progress/205.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def PeterScore():
-    a = random.uniform(1, 4)
-    print(a)
-
-
-PeterScore()
